9cc8069dfc241097b26bce00e372da11db0d5848
sihong Guo <yellowjerry6@gmail.com>
2020-03-02 10:48:07 -0600

    Swap function complete
    Swap with generating random num;
    
    BUG: Somethimes the Total happiness goes wrong, but the data array is fine (swap correctly but comes up with wrong happiness)

--- Something/src/main/java/com/example/App.java
+++ Something/src/main/java/com/example/App.java
@@ -8,29 +8,86 @@ public class App
 {
     
 
-	static String[] StudentName = {"1","2","3","4"};
+	// static String[] StudentName = {"1","2","3","4","5","6"};
 
-	static String[] A = {"2","3","4"};
-	static String[] B = {"1","3","4"};
-	static String[] C = {"1","2","4"};
-	static String[] D = {"1","2","3"};
+	// static String[] A = {"2","3","4"};
+	// static String[] B = {};
+	// static String[] C = {"1","2","4"};
+	// static String[] D = {"1","2","3"};
+	// static String[] E = {"2","3","6"};
+	// static String[] F = {"5","2","1"};
+
 	//happiness is 3,2,1
 
-	static String[][] StudentSet= {A,B,C,D};
+
+
+
+	//static String[] StudentName = {"1","3","4","2","6","5"};
+
+
+	static String[] StudentName = {"1","2","3","4","5","6"};
+
+	static String[] A = {"2","3","4"};
+	static String[] B = {};
+	static String[] C = {"1","2","4"};
+	static String[] D = {"1","2","3"};
+	static String[] E = {"2","3","6"};
+	static String[] F = {"5","2","1"};
+
+
+	//DATA TO HELP ASSERT DELETE SELF
+	// static String[] A = {"1","3","4"};
+	// static String[] B = {"2","2","4"};
+	// static String[] C = {"3","2","3"};
+	// static String[] D = {"4","2","3"};
+	// static String[] E = {"5","5","5"};
+	// static String[] F = {"6","2","1"};
+
+
+
+	static String[][] StudentSet= {A,B,C,D,E,F};
+
+	// static String[][] StudentSet= {A,C,D,B,F,E};
+
+
+	
 
 
 	public static void main( String[] args ){
-	    for (int i = 0; i< 4; i++){
-	    	System.out.println( StudentName[i] );
+	    	System.out.println( "we are in" );
+	    	//Swap(1,3);
+	}
+
+	public void RemoveDuplicate(String[] array, int choices){
+		for (int i = 0; i<choices-1; i++){
+			for (int j = i+1; j<choices; j++){
+				if (array[i] == array[j] && array[j] != "0"){
+					array[j] = "0";
+				}
+			}
+		}
+	}
+
+	public void RemoveSelf(String[] array, int choices){
+		for (int i = 0; i<6; i++){
+	    	for (int j = 0; j<3; j++){
+	    		if (StudentSet[i][j] == StudentName[i]){
+	    			StudentSet[i][j] ="0";
+	    		}
+	    	}
 	    }
 	}
 
 	// calculate the Group number
-	public int ReturnGroupNumber(int index, int teamsize){ 
+	public static int ReturnGroupNumber(int index, int teamsize){ 
 		return index / teamsize +1;
 	}
 
-	public String[] ReturnGroupMember(int index, int teamsize){
+	public static int AmountofGroups(int teamsize){
+		return StudentName.length/teamsize;
+	}
+
+	public static String[] ReturnGroupMember(int index, int teamsize){
 		int GroupNum = ReturnGroupNumber(index,teamsize);
 		String[] GourupMember = new String[10];
 
@@ -43,16 +100,22 @@ public class App
 		return GourupMember;
 	}
 
-	public int PersonalHappiness(int index, int teamsize, int choices){
+	public static int PersonalHappiness(int index, int teamsize, int choices){
 		String[] GourupMember = ReturnGroupMember(index,teamsize);
 		int happiness = 0;
+		int ifhavezero = 0;
+		if (StudentSet[index].length == 0){
+			return teamsize -1;
+		}
 		for (int i = 0; i < choices; i++)
 		{
-			for (int j = 0; j < teamsize; j++)
-				{
+			for (int j = 0; j < teamsize; j++){
 					if (StudentSet[index][i] == GourupMember[j]){
 						happiness += choices-i;
 					}
+				}
+			if (StudentSet[index][i] == "0"){
+					ifhavezero = 1;
 				}
 		}
 		return happiness;
@@ -60,7 +123,7 @@ public class App
 	}
 
 
-	public int TeamHappiness(int index, int teamsize, int choices){
+	public static int TeamHappiness(int index, int teamsize, int choices){
 		int GroupNum = ReturnGroupNumber(index,teamsize);
 		int startpoint = (GroupNum -1) * teamsize;
 		int happiness = 0;
@@ -71,9 +134,148 @@ public class App
 	}
 
 
+
+	public static int OverallHappiness(int teamsize, int choices){
+		int happiness = 0;
+		for (int i=1; i<=AmountofGroups(teamsize); i++){
+			int startpoint = (i -1) * teamsize;
+			happiness += TeamHappiness(startpoint,teamsize,choices);
+		}
+		return happiness;
+	}
+
+	public static double Variance(int teamsize,int choices){
+		double variance = 0;
+		int Amount = AmountofGroups(teamsize);
+		double mean = OverallHappiness(teamsize,choices) / Amount;
+		for (int i=1; i<=AmountofGroups(teamsize); i++){
+			int startpoint = (i -1) * teamsize;
+			// Sum the square of difference between mean and group happiness
+			variance += (TeamHappiness(startpoint,teamsize,choices) - mean) * (TeamHappiness(startpoint,teamsize,choices) - mean);
+		}
+		return variance / Amount;
+	}
+
 	public static double getRandom(double min, double max){
 	    double x = (int)(Math.random()*((max-min)+1))+min;
 	    return x;
+	}
+
+
+
+	//Swaping function to be called
+	public static void Swap(int Times,int choices){
+		for(int i=0; i<Times; i++){
+			int position1 = (int)getRandom(0,5);
+			int position2 = (int)getRandom(0,5);
+			int BackwardPercentage = (int)getRandom(0,100);
+			if (BackwardPercentage <= 3){
+				System.out.println( "Hit backwardButton" );
+				return;
+			}
+			while (position1 == position2){
+				position2 = (int)getRandom(0,5);
+			}
+			String Nameholder = StudentName[position2];
+			//make placeholder point to Position2
+			String[] Choiceholder = StudentSet[position2];
+			//pass values to positionholder to decide if keep track on the happiness
+	    	String[] position1holder = new String[10];
+	    	String[] position2holder = new String[10];
+	    	String position1nameholder = StudentName[position1];
+	    	String position2nameholder = StudentName[position2];
+	    	System.out.println( "position1 is" + Integer.toString(position1) );
+	    	System.out.println( "position2 is" + Integer.toString(position2) );
+	    	for (int j=0; j<choices; j++){	
+	    		for (int k=0; k<StudentSet[position1].length; k++){
+	    			position1holder[j] = StudentSet[position1][j];
+	    		}
+	    		for (int k=0; k<StudentSet[position2].length; k++){
+	    			position2holder[j] = StudentSet[position2][j];
+	    		}
+	    	}
+
+
+	    	for (int a=0;a<3;a++){
+	    		System.out.println( position1holder[a] );
+	    	}
+	    	System.out.println( "position1holder is " + position1 );
+
+	    	for (int a=0;a<3;a++){
+	    		System.out.println( position2holder[a] );
+	    	}
+	    	System.out.println( "position2holder is " + position2);
+
+
+
+
+	    	//temporary happiness and V
+	    	int temphappiness = OverallHappiness(2,3);
+	    	double tempvariance = Variance(2,3);
+	    	System.out.println( "temp OverallHappiness is" + Integer.toString(temphappiness) );
+	    	System.out.println( "temp Variance is" + Double.toString(tempvariance) );
+
+	    	//Swap
+	    	StudentSet[position2] = StudentSet[position1];
+	    	StudentSet[position1] = Choiceholder;
+
+	    	StudentName[position2] = StudentName[position1];
+	    	StudentName[position1] = Nameholder;
+
+
+	    	//happiness  and V after swap
+	    	int swaphappiness = OverallHappiness(2,3);
+	    	double swapvariance = Variance(2,3);
+
+	    	System.out.println( "swap OverallHappiness is" + Integer.toString(swaphappiness) );
+	    	System.out.println( "swap Variance is" + Double.toString(swapvariance) );
+
+
+	    	for (int a=0; a<StudentName.length; a++){
+	    		System.out.println( "Student: " + StudentName[a] );
+	    		for (int b=0; b<choices; b++){
+	    			if (StudentSet[a].length !=0){
+	    				System.out.println( StudentSet[a][b] );
+	    			}
+	    			else
+	    			{
+	    				System.out.println( "this set is NULL" );
+	    			}
+	    		}
+	    		System.out.println( "Next student" );
+	    	}
+
+	    	// keep if Overall is higher
+	    	if (swaphappiness >= temphappiness){
+	    		System.out.println( "Keep Result" );
+	    	}
+	    	else{
+	    		System.out.println( "Keep Previous Result" );
+	    		StudentName[position1] = position1nameholder;
+	    		StudentName[position2] = position2nameholder;
+	    		StudentSet[position1] = position1holder;
+	    		StudentSet[position2] = position2holder;
+	    	}
+
+	    	for (int a=0; a<StudentName.length; a++){
+	    		System.out.println( "Student: " + StudentName[a] );
+	    		for (int b=0; b<choices; b++){
+	    			if (StudentSet[a].length !=0){
+	    				System.out.println( StudentSet[a][b] );
+	    			}
+	    			else
+	    			{
+	    				System.out.println( "this set is NULL" );
+	    			}
+	    		}
+	    		System.out.println( "Next student" );
+	    	}
+
+
+
+		}
+
+		return;
 	}
 
 
--- Something/src/test/java/com/example/AppTest.java
+++ Something/src/test/java/com/example/AppTest.java
@@ -41,6 +41,82 @@ public class AppTest
     }
 
 
+    // TEST CASES ASSERT THAT DELETE SELF FROM PREFERENCE WORKS
+    // @Test
+    // public void AbnormalPreferenceFixTest1(){
+    //     String[] answer = {"0","0","0"};
+    //     app.RemoveSelf(app.E,3);
+    //     for (int i=0; i<3; i++){
+    //         assertEquals(answer[i], app.E[i]);
+    //     }
+    // }
+
+    // @Test
+    // public void AbnormalPreferenceFixTest2(){
+    //     String[] answer = {"0","3","4"};
+    //     app.RemoveSelf(app.A,3);
+    //     for (int i=0; i<3; i++){
+    //         assertEquals(answer[i], app.A[i]);
+    //     }
+    // }
+
+    // @Test
+    // public void AbnormalPreferenceFixTest3(){
+    //     String[] answer = {"0","0","4"};
+    //     app.RemoveSelf(app.B,3);
+    //     for (int i=0; i<3; i++){
+    //         assertEquals(answer[i], app.B[i]);
+    //     }
+    // }
+
+    // @Test
+    // public void AbnormalPreferenceFixTest4(){
+    //     String[] answer = {"0","2","0"};
+    //     app.RemoveSelf(app.C,3);
+    //     for (int i=0; i<3; i++){
+    //         assertEquals(answer[i], app.C[i]);
+    //     }
+    // }
+
+
+    // @Test
+    // public void AbnormalPreferenceFixTest5(){
+    //     String[] answer = {"0","2","3"};
+    //     app.RemoveSelf(app.D,3);
+    //     for (int i=0; i<3; i++){
+    //         assertEquals(answer[i], app.D[i]);
+    //     }
+    // }
+
+    // @Test
+    // public void AbnormalPreferenceFixTest6(){
+    //     String[] answer = {"0","2","1"};
+    //     app.RemoveSelf(app.F,3);
+    //     for (int i=0; i<3; i++){
+    //         assertEquals(answer[i], app.F[i]);
+    //     }
+    // }
+
+
+    // @Test
+    // public void AbnormalPreferenceFixTest2(){
+    //     String[] answer = {"5","0","0"};
+    //     app.RemoveDuplicate(app.E,3);
+    //     for (int i=0; i<3; i++){
+    //         assertEquals(answer[i], app.F[i]);
+    //     }
+    // }
+
+
+    @Test
+    public void GroupAmounttest1(){
+        assertEquals(3, app.AmountofGroups(2));
+    }
+
+    @Test
+    public void GroupAmounttest2(){
+        assertEquals(2, app.AmountofGroups(3));
+    }
 
     @Test
     public void GroupNumbertest(){
@@ -84,6 +160,15 @@ public class AppTest
             assertEquals(answer[0],case1[0]);
     }
 
+    @Test
+    public void GroupNumbertestcase4(){
+        String[] case1 = app.ReturnGroupMember(5,2);
+        String[] answer = {"5","6"};
+        for (int i = 0; i<2; i++){
+            assertEquals(answer[i],case1[i]);
+        }
+    }
+
 
     //test first person in group 1, A's happiness
     @Test
@@ -113,11 +198,34 @@ public class AppTest
         assertEquals (0, case1);
     }
 
+    // test personal happiness with not choice
+    @Test
+    public void Happinesstest5(){
+        int case1 = app.PersonalHappiness(5,2,3);
+        assertEquals (3, case1);
+    }
+
+    // test on "Bug" that put self into preference list
+    @Test
+    public void Happinesstest6(){
+        int case1 = app.PersonalHappiness(4,2,3);
+        assertEquals (1, case1);
+    }
+
+
+    // if input is empty
+    @Test
+    public void Happinesstest7(){
+        int case1 = app.PersonalHappiness(1,2,3);
+        assertEquals (1, case1);
+    }
+
+
     
     @Test
     public void TeamHappinesstest1(){
         int case1 = app.TeamHappiness(0,2,3);
-        assertEquals (6, case1);
+        assertEquals (4, case1);
     }
 
     @Test
@@ -127,6 +235,20 @@ public class AppTest
     }
 
     @Test
+    public void TeamHappinesstest3(){
+        int case1 = app.TeamHappiness(4,2,3);
+        assertEquals (4, case1);
+    }
+
+
+    @Test
+    public void OverallHappinesstest(){
+        int case1 = app.OverallHappiness(2,3);
+        assertEquals (10, case1);
+    }
+
+
+    @Test
     public void RandomNumTest1(){
         double case1 = app.getRandom(2,6);
         assertTrue (case1>=2);
@@ -134,6 +256,15 @@ public class AppTest
         assertEquals (0, case1 % 1, 0.001);
     }
 
+
+
+    @Test
+    public void SwapLogicTest(){
+        double case1 = app.OverallHappiness(2,3);
+        app.Swap(2,3);
+        double case2 = app.OverallHappiness(2,3);
+        assertTrue (case2 >= case1);
+    }
     
 
 }
